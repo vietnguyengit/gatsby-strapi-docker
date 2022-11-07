7757d907c259b9c84d88d4ce46d8bd75b9cb89b2
Viet Nguyen <vh.nguyen@utas.edu.au>
2022-11-07 12:46:27 +1100

    Config to run Vietnguyen dev stack on AWS Dev account

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,25 +1,42 @@
+require('dotenv').config({ path:`.env.${process.env.NODE_ENV}`});
 module.exports = {
   apps: [
     {
       name: 'myblog', // project name
       script: 'npm',
       args: 'start', // Script to start the Strapi server, `start` by default
-      env: {
-        APP_KEYS: 'keCGwpdo+1VigJlwUrM4vQ==,36yg1RGy2xwuwALVXLqO8A==,qQUfeoI5pJ15f5rD4gNyCA==,nAAgja9cfqWG3HqAW/YRhA==', // you can find it in your project .env file.
-        API_TOKEN_SALT: 'U4VVcwGeCbSTD+n35Nz+vA==',
-        ADMIN_JWT_SECRET: 'zk1Sv64fHDHbuHP0QNBW/g==',
-        JWT_SECRET: 'NabezrQIKQjWrrQeHnP62g==',
-        NODE_ENV: 'production',
-        DATABASE_HOST: 'myblog.cenezpxh4umh.ap-southeast-2.rds.amazonaws.com', // database Endpoint under 'Connectivity & Security' tab
-        DATABASE_PORT: '5432',
-        DATABASE_NAME: 'myblog', // DB name under 'Configuration' tab
-        DATABASE_USERNAME: 'postgres', // default username
-        DATABASE_PASSWORD: '5EN7ATBQkHIO48qwcwpq',
-        AWS_ACCESS_KEY_ID: 'AKIAU56QDQO2M2MNACOQ',
-        AWS_ACCESS_SECRET: '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', // Find it in Amazon S3 Dashboard
-        AWS_REGION: 'ap-southeast-2',
-        AWS_BUCKET_NAME: 'vietnguyen-myblog',
+      env_dev: {
+        APP_KEYS: process.env.APP_KEYS, // you can find it in your project .env file.
+        API_TOKEN_SALT: process.env.API_TOKEN_SALT,
+        ADMIN_JWT_SECRET: process.env.ADMIN_JWT_SECRET,
+        JWT_SECRET: process.env.JWT_SECRET,
+        NODE_ENV: process.env.NODE_ENV,
+        DATABASE_HOST: process.env.DATABASE_HOST, // database Endpoint under 'Connectivity & Security' tab
+        DATABASE_PORT: process.env.DATABASE_PORT,
+        DATABASE_NAME: process.env.DATABASE_NAME, // DB name under 'Configuration' tab
+        DATABASE_USERNAME: process.env.DATABASE_USERNAME, // default username
+        DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
+        AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+        AWS_ACCESS_SECRET: process.env.AWS_ACCESS_SECRET, // Find it in Amazon S3 Dashboard
+        AWS_REGION: process.env.AWS_REGION,
+        AWS_BUCKET_NAME: process.env.AWS_BUCKET_NAME,
       },
+      env_production: {
+        APP_KEYS: process.env.APP_KEYS,
+        API_TOKEN_SALT: process.env.API_TOKEN_SALT,
+        ADMIN_JWT_SECRET: process.env.ADMIN_JWT_SECRET,
+        JWT_SECRET: process.env.JWT_SECRET,
+        NODE_ENV: process.env.NODE_ENV,
+        DATABASE_HOST: process.env.DATABASE_HOST,
+        DATABASE_PORT: process.env.DATABASE_PORT,
+        DATABASE_NAME: process.env.DATABASE_NAME,
+        DATABASE_USERNAME: process.env.DATABASE_USERNAME,
+        DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
+        AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+        AWS_ACCESS_SECRET: process.env.AWS_ACCESS_SECRET,
+        AWS_REGION: process.env.AWS_REGION,
+        AWS_BUCKET_NAME: process.env.AWS_BUCKET_NAME,
+      }
     },
   ],
 };
