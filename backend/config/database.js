18cc84531db4bc17a8cec7ba6960f6a557f8febf
Viet Nguyen <vh.nguyen@utas.edu.au>
2022-11-07 12:34:17 +1100

    Config to run Vietnguyen dev stack on AWS Dev account

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", ""),
-      password: env("DATABASE_PASSWORD", ""),
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT"),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
     },
     useNullAsDefault: true,
   },
--- backend/config/server.js
+++ backend/config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', env('STRAPI_PORT')),
   app: {
     keys: env.array('APP_KEYS'),
   },
